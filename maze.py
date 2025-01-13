a9246c7b91976487fdf9d84d309fa4f787480870
Nilton Moura <moura.nilton@gmail.com>
2025-01-13 20:07:00 -0300

    added unit tests

--- maze.py
+++ maze.py
@@ -4,7 +4,16 @@ from cell import Cell
 
 
 class Maze():
-    def __init__(self, x1, y1, num_rows, num_cols, cell_size_x, cell_size_y, win):
+    def __init__(
+            self,
+            x1,
+            y1,
+            num_rows,
+            num_cols,
+            cell_size_x,
+            cell_size_y,
+            win=None,
+        ):
         self._x1 = x1
         self._y1 = y1
         self._num_rows = num_rows
@@ -13,6 +22,8 @@ class Maze():
         self._cell_size_y = cell_size_y
         self._win = win
         self._cells = []
+
+        self._create_cells()
 
     def _create_cells(self):
         for i in range(self._num_cols):
@@ -23,6 +34,8 @@ class Maze():
                 self._draw_cell(i, j)
 
     def _draw_cell(self, i, j):
+        if self._win is None:
+            return
         x1 = i * self._cell_size_x + self._x1
         x2 = x1 + self._cell_size_x
         y1 = j * self._cell_size_y + self._y1
@@ -31,5 +44,7 @@ class Maze():
         self._animate()
 
     def _animate(self):
+        if self._win is None:
+            return
         self._win.redraw()
         time.sleep(0.05)
