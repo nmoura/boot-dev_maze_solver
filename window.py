95715d393c989d756c2910f2d6a5dd1bcf016429
Nilton Moura <moura.nilton@gmail.com>
2025-01-13 13:09:47 -0300

    refactor Cell class to initialize it without params, but when drawing

--- window.py
+++ window.py
@@ -45,36 +45,32 @@ class Line():
 
 
 class Cell():
-    def __init__(self, p1, p2, win):
+    def __init__(self, win):
         self.has_left_wall = True
         self.has_right_wall = True
         self.has_top_wall = True
         self.has_bottom_wall = True
-        self._x1 = p1.x
-        self._x2 = p2.x
-        self._y1 = p1.y
-        self._y2 = p2.y
+        self._x1 = None
+        self._x2 = None
+        self._y1 = None
+        self._y2 = None
         self._win = win
         self._wall_color = "black"
 
-    def draw(self):
+    def draw(self, x1, y1, x2, y2):
+        self._x1 = x1
+        self._x2 = x2
+        self._y1 = y1
+        self._y2 = y2
         if self.has_left_wall:
-            lw_p1 = Point(self._x1, self._y1)
-            lw_p2 = Point(self._x1, self._y2)
-            left_line = Line(lw_p1, lw_p2)
-            self._win.draw_line(left_line, self._wall_color)
+            line = Line(Point(x1, y1), Point(x1, y2))
+            self._win.draw_line(line, self._wall_color)
         if self.has_top_wall:
-            tw_p1 = Point(self._x1, self._y1)
-            tw_p2 = Point(self._x2, self._y1)
-            top_line = Line(tw_p1, tw_p2)
-            self._win.draw_line(top_line, self._wall_color)
+            line = Line(Point(x1, y1), Point(x2, y1))
+            self._win.draw_line(line, self._wall_color)
         if self.has_right_wall:
-            rw_p1 = Point(self._x2, self._y1)
-            rw_p2 = Point(self._x2, self._y2)
-            right_line = Line(rw_p1, rw_p2)
-            self._win.draw_line(right_line, self._wall_color)
+            line = Line(Point(x2, y1), Point(x2, y2))
+            self._win.draw_line(line, self._wall_color)
         if self.has_bottom_wall:
-            bw_p1 = Point(self._x1, self._y2)
-            bw_p2 = Point(self._x2, self._y2)
-            bottom_line = Line(bw_p1, bw_p2)
-            self._win.draw_line(bottom_line, self._wall_color)
+            line = Line(Point(x1, y2), Point(x2, y2))
+            self._win.draw_line(line, self._wall_color)
